f582e864f19fc514850ef3899decd65294bc2c9d
Phan Hiếu <mrphanhieuqt@gmail.com>
2015-09-24 16:40:33 +0700

    Fighting Game Demo: Add Health Bar

--- 2.Coding/FightingDemo/Assets/Scripts/MainController.cs
+++ 2.Coding/FightingDemo/Assets/Scripts/MainController.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Man : MonoBehaviour {
+public class MainController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+	void OnGUI() {
+	}
+
 }
---
+++ 2.Coding/FightingDemo/Assets/Scripts/Model/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+
+	public GameObject InnerHealthBar;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setInnerHealthBarWidth(float width) {
+		SpriteRenderer sr = InnerHealthBar.GetComponent<SpriteRenderer> ();
+		float prevWidth = sr.bounds.size.x;
+		sr.transform.localScale = new Vector2(width,1f);
+		float curWidth = sr.bounds.size.x;
+		float posX = sr.transform.position.x;
+		float posY = sr.transform.position.y;
+		sr.transform.position = new Vector2 (posX - (prevWidth - curWidth)/2, posY);
+	}
+}
